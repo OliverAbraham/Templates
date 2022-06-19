674dd2e56d721c000606bd0c07947355083ec28f
Oliver Abraham <mail@oliver-abraham.de>
2022-06-19 15:04:54 +0200

    improvements

--- ConsoleAppTemplate/ConsoleAppTemplate/Configuration.cs
+++ ConsoleAppTemplate/ConsoleAppTemplate/Configuration.cs
@@ -1,19 +1,18 @@
 ﻿using NLog;
 
-namespace ConsoleAppTemplate
-{
-    public class Configuration
-    {
-        public string Option1   { get; set; }
-        public string Option2   { get; set; }
-        public string Option3	{ get; set; }
+namespace ConsoleAppTemplate;
 
-        public void LogOptions(ILogger logger)
-        {
-            //Note: To align the output in columns, set visual studio to use spaces instead of tabs!
-            logger.Debug($"Option1                        : {Option1}");
-            logger.Debug($"Option2                        : {Option2}");
-            logger.Debug($"Option3                        : {Option3}");
-        }
+public class Configuration
+{
+    public string Option1   { get; set; }
+    public string Option2   { get; set; }
+    public string Option3	{ get; set; }
+
+    public void LogOptions(ILogger logger)
+    {
+        //Note: To align the output in columns, set visual studio to use spaces instead of tabs!
+        logger.Debug($"Option1                        : {Option1}");
+        logger.Debug($"Option2                        : {Option2}");
+        logger.Debug($"Option3                        : {Option3}");
     }
 }
